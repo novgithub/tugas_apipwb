33fecda0c528d8f85768c57cc08a18296bd1d1da
novgithub <dahyunnie1234@gmail.com>
2021-01-14 10:37:37 +0700

    first commit

---
+++ app/Http/Controllers/SiswaController.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Siswa;
+use Illuminate\Http\Request;
+
+class SiswaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $siswa = Siswa::all();
+        return response()->json([
+            'status' => 'Founded',
+            'code' => 200,
+            'data' => $siswa,
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create(Request $request)
+    {
+        $siswa = new Siswa;
+        $siswa->nama = $request->nama;
+        $siswa->alamat = $request->alamat;
+        $siswa->save();
+        
+        return "Data Berhasil Masuk";
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Siswa  $siswa
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Siswa $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Siswa  $siswa
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Siswa $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Siswa  $siswa
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+            $nama = $request->nama;
+            $alamat = $request->alamat;
+     
+            $siswa = Siswa ::find($id);
+            $siswa->nama = $nama;
+            $siswa->alamat = $alamat;
+            $siswa->save();
+     
+            return "Data Berhasil Di Update";
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Siswa  $siswa
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $siswa = Siswa::find($id);
+        $siswa->delete();
+        
+        return "Data Berhasil Di Delete";
+    }
+}
